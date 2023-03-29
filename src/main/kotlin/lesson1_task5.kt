3c7a990e7dafe9ea4315991f1e48179a13f1f721
Daniel <falcon0852z@gmail.com>
2023-03-29 23:24:25 +0300

    Task done.

---
+++ src/main/kotlin/lesson1_task5.kt
@@ -0,0 +1,28 @@
+fun main() {
+//    val year: Int = 1961
+//    var hour: String = "09"
+//    var minute: String = "07"
+//    println("""$year
+//        |$hour
+//        |$minute
+//    """.trimMargin())
+//    hour = "10"
+//    minute = "55"
+//    println("$hour:$minute")
+
+//    val length: Long = 40868600000
+//    val age: Byte = 27
+//    val partOfDay: Float = 0.075f
+      val seconds: Short = 6480
+//    val partOfYear: Double = 2.0547945205479453E-4
+//    val apogee: Int = 327000
+//    println("""$length
+//        |$age
+//        |$partOfDay
+//        |$seconds
+//        |$partOfYear
+//        |$apogee
+//    """.trimMargin())
+    val minutes = seconds/60
+    println("$minutes:00")
+}
