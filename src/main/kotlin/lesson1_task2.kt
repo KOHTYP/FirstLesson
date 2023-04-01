144077060bc1076840a936785109a76b978c4ee5
Daniel <falcon0852z@gmail.com>
2023-04-01 15:48:51 +0300

    KS-1-2 Branch fixed

---
+++ src/main/kotlin/lesson1_task2.kt
@@ -0,0 +1,9 @@
+fun main() {
+    var amountOfOrders: Int = 75
+    val gratitudeForPurchase: String = "Thank you for your purchase!"
+    println("$amountOfOrders $gratitudeForPurchase")
+    var workers: Int = 2000
+    //println(workers)
+    workers = 1999
+    println(workers)
+}
