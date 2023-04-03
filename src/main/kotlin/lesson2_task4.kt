243ccc4710f389398846880523814798752240ad
Daniel <falcon0852z@gmail.com>
2023-04-03 21:23:38 +0300

    KS-2-4 done.

---
+++ src/main/kotlin/lesson2_task4.kt
@@ -0,0 +1,6 @@
+fun main() {
+    val crystal: Double = 7.0
+    val iron: Double = 11.0
+    println("The buff for crystals will give you ${(crystal / 100 * 20).toInt()} additional crystals.")
+    println("The buff for iron will give you ${(iron / 100 * 20).toInt()} additional iron.")
+}
